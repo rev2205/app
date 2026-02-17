1ba8567d296948b6fa40223451ea2ab9930e47e5
revanth <revanthreddymalapati@gmail>
2026-02-17 20:47:10 +0530

    fix: update base path to /app/ for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/musicApp/',
+  base: '/app/',
   plugins: [react()],
   server: {
     host: '127.0.0.1',
